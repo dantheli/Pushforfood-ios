8560c92a97e4b132c9eaee9563344c55e35bb356
Daniel Li <dl743@cornell.edu>
2016-06-24 22:08:31 -0400

    Initial xcode commit

---
+++ Food/Network.swift
@@ -0,0 +1,13 @@
+//
+//  Network.swift
+//  Food
+//
+//  Created by Daniel Li on 6/24/16.
+//  Copyright © 2016 MEDJI. All rights reserved.
+//
+
+import Foundation
+
+class Network {
+    
+}
---
+++ Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Daniel Li on 6/24/16.
+//  Copyright © 2016 MEDJI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
